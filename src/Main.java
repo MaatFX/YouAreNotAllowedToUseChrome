9066905b6b10cb4725e1b51311aa17051d938067
JBinaryLogic <JBinaryLogic@null.spigotmc.org>
2018-12-04 19:25:05 +0100

    first version

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.stream.Stream;
+
+public class Main {
+
+    private final static int REFRESH = 5 * 1000; // in ms
+
+    public static void main(String... args) throws IOException {
+
+        TimerTask timerTask = new TimerTask() {
+
+            @Override
+            public void run() {
+
+                Stream<ProcessHandle> processHandleStream = ProcessHandle.allProcesses();
+
+                processHandleStream.forEach(processHandle -> {
+                    if (processHandle.info().command().toString().contains("chrome.exe")) {
+                        processHandle.destroy();
+                    }
+                });
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(timerTask, 0, REFRESH);
+
+    }
+}
